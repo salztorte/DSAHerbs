ded3cd5009fe969dd7f8714db07d31ed76e13d99
Salztorte <olli.franzen@gmx.de>
2016-01-21 22:22:40 +0100

    test added

---
+++ test/components/search.spec.js
@@ -0,0 +1,7 @@
+//"use strict";
+//import expect from 'expect'
+//import React from 'react'
+//import TestUtils from 'react-addons-test-utils'
+//import Counter from '../../src/components/search.jsx'
+//
+//
---
+++ test/test.spec.js
@@ -0,0 +1,36 @@
+"use strict";
+import expect from 'expect';
+
+describe("tests", function() {
+	it("should test", function(){
+		expect(1).toBe(1);
+	});
+
+	it("should be able to write to console", function() {
+		expect(console).toNotBe(null);
+		console.log("ConsoleTest", "Hello World!");
+	});
+});
+//
+//describe("JavaScript", function() {
+//	it("should be able to write to typed Arrays given by val", function() {
+//		function changeArray(buffer){
+//			buffer[0] = 1;
+//			buffer[1] = 2;
+//			buffer[2] = 3;
+//		}
+//		var buf = new Uint32Array(3);
+//		changeArray(buf);
+//
+//		var one = buf[0];
+//		var two = buf[1];
+//		var three = buf[2];
+//
+//		expect(buf).to.have.length(3);
+//		expect(one).to.be(1);
+//		expect(two).to.be(2);
+//		expect(three).to.be(3);
+//
+//
+//	});
+//});
