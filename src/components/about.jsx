03dfa38a54faa957d9c3cd8742b8d8cf9a550f05
FRA <o.franzen@uts.de>
2016-01-21 16:46:48 +0100

    ..

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,25 +3,27 @@ import React, { Component } from "react";
 import {BaseComponent , Content} from "./tools.jsx"
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
-
+import TextField from 'material-ui/lib/text-field';
 
 class About extends BaseComponent {
     render(){
         return (
-            <Content title="About">
-                <List>
-                    DSA Herps - Version 0.0
-                </List>
-                <List>
-                    Copyright © 2016 von Oliver Franzen
-                </List>
-                <List>
-                    Kontakt : <a href= "mailto:salztorte92@gmail.com">salztorte92@gmail.com</a>
-                </List>
-            </Content>
+            <TextField defaultValue="" />
         );
     }
 }
 
 
 export default About;
+//
+//<Content title="About">
+//        <List disabled={true}>
+//            DSA Herps - Version 0.0
+//        </List>
+//        <List>
+//            Copyright © 2016 von Oliver Franzen
+//        </List>
+//        <List>
+//            Kontakt : <a href= "mailto:salztorte92@gmail.com">salztorte92@gmail.com</a>
+//        </List>
+//    </Content>
