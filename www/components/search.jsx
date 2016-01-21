6a20a77312157eec4c8c2fc7a82d642eadaaadeb
FRA <o.franzen@uts.de>
2016-01-21 09:59:41 +0100

    ..

--- www/components/search.jsx
+++ www/components/search.jsx
@@ -5,42 +5,69 @@ import SelectField from 'material-ui/lib/select-field';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import { PLANTS_TYPES } from '../constans.jsx';
 
-let createWrapper = function(wrapper,itemsJson){
+let createWrapper = function(itemsJson){
     let resultArr = [];
+    resultArr.push(
+        React.createElement(MenuItem, {
+            label : " ",
+            key : " ",
+            primaryText : "-",
+            value : null
+
+        })
+    );
+
     for(var item in itemsJson){
-        let ele = React.createElement(wrapper, {
+        let ele = React.createElement(MenuItem, {
             key : item,
             primaryText : item,
             value : item
         });
         resultArr.push(ele);
-    };
+    }
+    ;
     return resultArr;
 };
 
 class Search extends BaseComponent {
     constructor(props){
         super(props);
-        this.state = {plantValue : null};
-        this._bind("_handleChange");
+        let initValue = {
+            value : {
+                "plant" : null,
+                "poison" : null,
+                "means" : null,
+                "others" : null
+            }
+        }
+        this.state = initValue;
+        this._bind("_handleChange", "_updateState");
     }
 
     _handleChange(event, index, value){
-        this.setState({"plantValue":value});
+        this._updateState("plant", value);
+    }
+
+    _updateState(name, value){
+        let newState = this.state;
+        newState.value[name] = value
+        this.setState(newState);
     }
 
     render(){
         return (
             <div>
                 <Toolbar title="DSAHerps"/>
-                <SelectField value={this.state.plantValue} onChange={this._handleChange} floatingLabelText="Pflanzenauswahl">
-                    {createWrapper(MenuItem, PLANTS_TYPES)}
+                <SelectField value={this.state.value.plant} onChange={this._handleChange}
+                             floatingLabelText="Pflanzenauswahl">
+                    {createWrapper(PLANTS_TYPES)}
                 </SelectField>
             </div>
         );
     }
 
-};
+}
+;
 
 
 export default Search;
