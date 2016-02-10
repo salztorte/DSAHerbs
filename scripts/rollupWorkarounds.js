802239ee2f2e494f88d0c882ad7e9100c0ae3401
FRA <o.franzen@uts.de>
2016-02-10 12:25:54 +0100

    change scripts

--- scripts/rollupWorkarounds.js
+++ scripts/rollupWorkarounds.js
@@ -1,19 +1,29 @@
 var fs = require('fs');
 
-var reactRouterPath = 'node_modules/react-router/package.json';
-var routerPjson = JSON.parse(fs.readFileSync(reactRouterPath, 'utf8'));
-routerPjson['jsnext:main'] = '';
 
-fs.writeFile(reactRouterPath, JSON.stringify(routerPjson, null, 4), 'utf8', function (err) {
-    if (err) return console.log(err);
-});
+var reactRouterWA = function(){
+    var reactRouterPath = 'node_modules/react-router/package.json';
+    var routerPjson = JSON.parse(fs.readFileSync(reactRouterPath, 'utf8'));
+    routerPjson['jsnext:main'] = '';
+
+    fs.writeFile(reactRouterPath, JSON.stringify(routerPjson, null, 4), 'utf8', function(err){
+        if(err) return console.log(err);
+    });
+};
 
 
-var pathPjson = 'node_modules/react-redux/package.json';
+var reactReduxWA = function(){
+    var pathPjson = 'node_modules/react-redux/package.json';
 
-var rrPjson = JSON.parse(fs.readFileSync(pathPjson, 'utf8'));
-rrPjson['jsnext:main']= 'src/index.js';
+    var rrPjson = JSON.parse(fs.readFileSync(pathPjson, 'utf8'));
+    rrPjson['jsnext:main']= 'src/index.js';
 
-fs.writeFile(pathPjson, JSON.stringify(rrPjson, null, 4), 'utf8', function (err) {
-    if (err) return console.log(err);
-});
+    fs.writeFile(pathPjson, JSON.stringify(rrPjson, null, 4), 'utf8', function (err) {
+        if (err) return console.log(err);
+    });
+};
+
+
+
+reactRouterWA();
+reactReduxWA();
