4e37825bff49c1dba47b8dcd470c84da65143c2d
salztorte <olli.franzen@gmx.de>
2016-02-23 07:52:25 +0100

    ..

--- src/database.js
+++ src/database.js
@@ -1,42 +1,39 @@
+
+let cmp = (item, searchTerm) => {
+    if (Object.keys(searchTerm).length == 1) {
+        let key = Object.keys(searchTerm)[0];
+        return singelTrait(item[key], searchTerm[key]);
+    } else {
+        return multiTrait(item, searchTerm);
+    }
+};
+let singelTrait = (trait, searchTrait) => {
+    if (Array.isArray(trait))
+        trait = trait.join(';');
+
+    return trait.toLowerCase().indexOf(searchTrait.toLowerCase()) != -1;
+};
+let multiTrait = (item, searchTerm) => {
+    return Object.keys(searchTerm).reduce((bool, key) => {
+        return (bool) ? singelTrait(item[key], searchTerm[key]) : false;
+    }, true);
+};
+
+
 const JsonQuery = function (json) {
-    let data = json;
-
-    this.query = function (searchTerm = []) {
-        if (searchTerm.length == 0) return data;
-
-        return data.reduce((result, item) => {
+    this.data = json;
+};
+JsonQuery.prototype.query = function(searchTerm = []){
+    if (searchTerm.length == 0){
+        return this.data;
+    }else{
+        return this.data.reduce((result, item) => {
             if (cmp(item, searchTerm))
                 result.push(item);
             return result;
         }, []);
-    };
-
-    let cmp = (item, searchTerm) => {
-        if (Object.keys(searchTerm).length == 1) {
-            let key = Object.keys(searchTerm)[0];
-            return singelCmp(item[key], searchTerm[key]);
-        } else {
-            return multiCmp(item, searchTerm);
-        }
-    };
-
-    let singelCmp = (trait, searchTrait) => {
-        if (Array.isArray(trait))
-            trait = trait.join(';');
-
-        return trait.toLowerCase().indexOf(searchTrait.toLowerCase()) != -1;
-    };
-
-    let multiCmp = (item, searchTerm) => {
-        return Object.keys(searchTerm).reduce((bool, key) => {
-            return (
-                (bool) ? singelCmp(item[key], searchTerm[key]) : false
-            );
-        }, true);
-    };
-
+    }
 };
-
 
 export default JsonQuery;
 
